6dd6edb9661eae1dbcd304395704594c99ef16b6
nimitarora <nimitarora7@gmail.com>
2020-12-13 15:11:47 +0530

    Basics

---
+++ src/main/scala/com/rockthejvm/FunctionalProgramming.scala
@@ -0,0 +1,104 @@
+package com.rockthejvm
+
+object FunctionalProgramming extends App{
+
+  val simpleIncrementer = new Function1[int, int] //takes int and returns int
+  {
+    override def apply(args: Int) : Int = args + 1
+  }
+  simpleIncrementer(23)   //24
+  // all scala functions are instances of these FunctionX types
+
+  val stringConcatenator = new function2[String, String, String]  //takes 2 strings and returns 1 string
+  {
+    override def apply(arg1: String, arg2: String): String = arg1+arg2
+  }
+  stringConcatenator("My name is", "Nimit")
+
+  //syntax sugars
+  val doubler : new Function1[Int,Int] = (x:Int) => 2 * x
+  doubler(4)  //8
+  /*
+  equivalent to much longer
+    new Function1[Int,Int]
+    {
+      override def apply(x: Int): Int = 2*x
+    }
+  */
+
+  val doubler: Int => Int = (x:Int) => 2*x
+
+  //HIGHER ORDER FUNCTIONS
+
+  val aMappedList = List(1,2,3).map(x => x+1)
+  //returns another List i.e. List(2,3,4)
+
+  val flatMappedList = List(1,2,3).flatMap(x => List(x,2*x))
+  //result will be concatenated in a single list
+  //List(1,2,2,4,3,6)
+
+  val filteredList = List(1,2,3,4,5).filter(x=> x <3)
+  val filteredList1 = List(1,2,3,4,5).filter(_ x <3)
+  //result List(1,2)
+
+  // all pairs between numbers 1,2,3 and letters 'a','b','c'
+  val allPairs = List(1,2,3).flatMap(numbers => List('a', 'b', 'c').map(letters => s"$numbers-$letters"))
+  //result - List(1-a, 1-b, 1-c, 2-a, 2-b, 2-c, 3-a ,3-b, 3-c)
+  //alternative
+  val alternativePair = for{
+    numbers <- List(1,2,3)
+    letters <- List('a', 'b', 'c')
+  }yield s"$numbers-$letters"
+  //equivalent to map/flatmap chain above
+  //compiler converts for to map/flatmap chain
+
+  /*
+  Collections
+   */
+
+  //LISTS
+  val aList = List(1,2,3,4,5)
+  val firstElement = aList.head   // 1
+  val rest = aList.tail       //List(2,3,4,5)
+  val aPrependList = 0 :: aList     // List(0,1,2,3,4,5)
+  val anExtendedList = 0 +: aList :+ 6    //List(0,1,2,3,4,5,6)
+
+  //SEQUENCES
+  val aSequence: Seq[Int] = Seq(1,2,3)
+  val accessElement = aSequence(1)    // element at index 1 i.e. 2
+
+  //VECTORS : fast Seq implementation
+  val aVector = Vector(1,2,3,4,5)
+
+  //SETS
+  val aSet = Set(1,2,3,4,1,2,3)       // Set(1,2,3,4)
+  val has5 = aSet.contains(5)       // false
+  val addValue = aSet + 5         // Set(1,2,3,4,5)
+  val removeValue = aSet - 3       // Set(1,2,4)
+
+  //RANGES
+  val aRange = 1 to 1000
+  val twoby2 = aRange.map(x => x*2).toList      //List(2,4,6,8,...)
+
+  //TUPLES
+  val aTuple = ("Nimit", "Rohit". 3242)
+
+  //MAPS
+  val aPhoneBook: Map[String, Int] = Map(
+    "John", 8349873
+    "Daniel" -> 874923
+  )
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
--- src/main/scala/com/rockthejvm/ObjectOrientation.scala
+++ src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -36,16 +36,98 @@ object ObjectOrientation extends App{
     def eat(animal: Animal): Unit
   }
 
+  trait Philospher{
+    def ?!(thought: String): Unit // ?!, !, ? are VALID method names
+  }
+
   //single-class inheritance with multi-trait = "mixing"
-  class Crocodile extends Animal with Carnivore { // i.e. scala provides multi class/interface inheritance
+  class Crocodile extends Animal with Carnivore with Philospher { // i.e. scala provides multi class/interface inheritance
     override def eat(animal: Animal) = println("I'm eating you")
+
+    override def ?!(thought: String): Unit = println(s"I am thinking about $thought")
   }
 
   val aCroc = new Crocodile
   aCroc.eat(aDog)
   aCroc eat aDog  //infix notation = object method argument, only available with method with one arg
+  aCroc ?! "Swimming"
 
+  // ##operators in scala are actually methods## //
+  val basicMath = 1+2
+  val anotherbasicMath = 1 +(2) //both are equivalent
 
+  //anonymous function
+  val dinosaur = new Carnivore {
+    override def eat(animal: Animal): Unit = println("Anonymous class")
+  }/*
+   Compiler -
+   class Carnivore_Anonymous_43554 extends Carnivore{
+    override def eat(animal: Animal): Unit = println("Anonymous class")
+   }
+   and then intiatiate it
+   val dinosaur = new Carnivore_Anonymous_43554
+   */
 
+  //SINGLETON OBJECT
+  object MySingleton {    // it is the only instance of MySingleton type
+    val myVal = 4353
+    def mySpecialMethod(): Int = 3452
+    def apply(x: Int): Int = x + 1    //special method available in scala
+  }
+  MySingleton.mySpecialMethod()     // to call method in SINGLETON object
+
+  MySingleton.apply(34)
+  MySingleton(34)     // equivalent to MySingleton.apply(34)
+
+  //Singleton object and class of same name can exist in same file and called COMPANIONS
+  //COMPANIONS can access fields and methods of each other
+
+  object Animal {
+    // Singleton Animal and instances of Animal class are different things
+    val canLiveInfinitely = false
+  }
+  val animalsCanLiveInfinitely = Animal.canLiveInfinitely //accessed in same way as static methods or fields are accessed in C++
+  
+  /*
+  case classes = lightweight data structures with some boilerplate
+  - sensible equals and hash code
+  - serialization
+  - contains companion with apply
+  - pattern matching
+   */
+
+  case class Person(Name : String, Age: Int)
+
+  // case class contains apply in companion which instantiates below without using new keyword
+  // can be constructed without the keyword new
+  val zack = Person("Zack", 16)     //Person.apply("Bob",18)
+
+  //EXCEPTIONS
+  try{
+    // code that can throw an exception
+    val x:String = null
+    x.length
+  }catch{
+    case e: Exception => "SOme error message"
+  }finally {
+    // execute some code no matter what
+  }
+
+  //GENERICS
+  abstract class MyList[T]
+  {
+    def head: T
+    def tail: MyList[T]
+  }
+
+  //LIST
+  //using generic type with a concrete type
+  val aList : List[Int] = List(1,2,3)
+  val first = aList.head
+  val rest = aList.tail
+
+  //Point 1: in scala we usually operate with IMMUTable values/objects
+
+  //Point 2: Scala is closest to Object Oriented idea
 
 }
